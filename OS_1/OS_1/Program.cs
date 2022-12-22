1e341026e1f857325efd6ff23ef0119a0484a119
Quickscoper <nikitamarkov2u@gmail.com>
2022-12-22 14:05:10 +0500

    Добавлена Л1.

---
+++ OS_1/OS_1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace OS_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Quickscoper\Documents\OS_1\OS_1\OSl1.docx";
+            Process process = new Process();
+
+            process.StartInfo.FileName = path;
+            process.StartInfo.UseShellExecute = true;
+
+            if (process.Start())
+            { Console.WriteLine("Процесс запущен успешно."); }
+            else
+            { Console.WriteLine("Процесс не запущен."); }
+
+            process.WaitForExit();
+
+            Console.WriteLine("Процесс завершён.");
+
+        }
+    }
+}
