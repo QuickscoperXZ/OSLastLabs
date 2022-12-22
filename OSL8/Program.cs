395e4f12f37d42892b63a66ad590af0a99606fc4
Quickscoper <nikitamarkov2u@gmail.com>
2022-12-22 14:05:00 +0500

    Добавлена Л8.

---
+++ OSL8/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+
+namespace OSL8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool runThreads = true;
+            Queue queue = new Queue(10);
+            for (int i = 0; i <= 2; i++)
+            {
+                Thread wr = new Thread(() =>
+                {
+                    Random rnd = new Random();
+                    int val;
+                    while (runThreads)
+                    {
+                        val = rnd.Next(-500, 500);
+                        queue.Enqueue(val);
+                        Console.WriteLine($"{Thread.CurrentThread.Name}: Добавлено: {val} ");
+                    }
+                });
+                wr.Name = $"WRITER-{i}";
+                wr.Start();
+            }
+            for (int i = 0; i <= 2; i++)
+            {
+                Thread rd = new Thread(() =>
+                {
+                    while (runThreads)
+                    {
+                        Console.WriteLine($"{Thread.CurrentThread.Name}: Изъято: {queue.queueFirst()}");
+                        queue.Dequeue();
+                    }
+                });
+                rd.Name = $"READER-{i}";
+                rd.Start();
+            }
+            Console.ReadKey();
+            runThreads = false;
+        }
+    }
+}
---
+++ OSL8/Queue.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+public class Queue
+{
+	private int first, last, size;
+	private int[] queue;
+
+	public Queue(int c)
+	{
+		first = last = 0;
+		size = c;
+		queue = new int[size];
+	}
+
+	public void Enqueue(int data)
+	{
+		lock (this)
+		{
+			if (size == last)
+			{
+				Console.Write("Очередь заполнена.");
+				return;
+			}
+
+			else
+			{
+				queue[last] = data;
+				last++;
+			}
+			return;
+		}	
+	}
+
+	public void Dequeue()
+	{
+        lock (this)
+        {
+
+            if (first == last)
+			{
+			Console.Write("Очередь пуста.");
+			return;
+			}
+
+			else
+			{
+			for (int i = 0; i < last - 1; i++)
+			{
+				queue[i] = queue[i + 1];
+			}
+
+			if (last < size)
+				queue[last] = 0;
+
+			last--;
+			}
+            return;
+        }
+	}
+
+	public void queueDisplay()
+	{
+		int i;
+		if (first == last)
+		{
+			Console.Write("Очередь пуста.");
+			return;
+		}
+
+		for (i = first; i < last; i++)
+		{
+			Console.Write($"{queue[i]}, ");
+		}
+		return;
+	}
+	public string queueFirst()
+	{
+		if (first == last)
+		{
+			return "Очередь пуста.";
+		}
+		return Convert.ToString(queue[first]);
+	}
+}
