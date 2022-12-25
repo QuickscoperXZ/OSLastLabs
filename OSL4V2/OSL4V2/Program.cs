1bdbf79cf5da22f8bec1d01672e59c52a45a25cf
Quickscoper <nikitamarkov2u@gmail.com>
2022-12-25 05:29:44 +0500

    Добавлены четвёртая и пятые лабы

---
+++ OSL4V2/OSL4V2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace OSL4V2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            Thread ft = new Thread(delegate() { FirstThread.PrintIn(new FileInfo(@"C:\Users\Quickscoper\Documents\OS_Labs\OSL4V2\OSL4V2\result\res.dat"), -10, 1, 1000, 10); });
+            Thread st = new Thread(delegate () { new SecondThread().GetSize(new FileInfo(@"C:\Users\Quickscoper\Documents\OS_Labs\OSL4V2\OSL4V2\result\res.dat")); });
+            ft.Start();
+            st.Start();
+        }
+    }
+    internal class FirstThread
+    {
+        public static void PrintIn(FileInfo info, int startPoint, int step, int sleepTime, int count)
+        {
+            StreamWriter writer = new StreamWriter(info.FullName,true);
+            writer.WriteLine($"Запуск от: {DateTime.Now}");
+            writer.Close();
+            for (int i = 1; i <+ count+1; i++)
+            {
+                StreamWriter fw = new StreamWriter(info.FullName, true);
+                fw.WriteLineAsync($"{count}|{Math.Sin(startPoint + count * step)}");
+                fw.Close();
+                Thread.Sleep(sleepTime);
+            }
+        }
+    }
+    internal class SecondThread
+    {
+            
+        void ctrlcpressed(object sender, ConsoleCancelEventArgs e) { Console.WriteLine("Завершение....."); }
+        public SecondThread()
+        {
+            Console.CancelKeyPress += new ConsoleCancelEventHandler(ctrlcpressed);
+        }
+        long lastSize;
+        public void GetSize(FileInfo info)
+        {
+            lastSize = info.Length;
+            while (true)
+            {
+                info.Refresh();
+                if (lastSize != info.Length)
+                {
+                    Console.WriteLine(info.Length);
+                    lastSize = info.Length;
+                }
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
---
+++ OSLab5/OSLab5/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading;
+
+namespace OSLab5
+{
+    // -0,09353640519196606
+    // 0,43143169870877246
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Variables variables = new Variables();
+            Thread th1 = new Thread(delegate () { variables.FirstMethod(); });
+            Thread th2 = new Thread(delegate () { variables.SecondMethod(); });
+
+            th1.Start();
+            th2.Start();
+
+            Thread.Sleep(10000);
+            Console.WriteLine(variables.y);
+        }
+    }
+    class Variables
+    {
+        public Variables()
+        {
+            x = 0;
+            y = 0;
+        }
+        public double x, y;
+        Mutex mObj = new Mutex();
+        public void FirstMethod()
+        {
+           //await Task.Run(() => 
+            //{ 
+                for (int i = 0; i < 10; i++)
+                {
+                    //mObj.WaitOne();
+                    Thread.Sleep(40);
+                    y = Math.Sin(Math.Pow(x, 2) + Math.Cos(2 * y));
+                    //mObj.ReleaseMutex();
+                }
+            //});
+        }
+        public void SecondMethod()
+        {
+            //await Task.Run(() => 
+            //{ 
+                for (int i = 0; i < 10; i++)
+                {
+                    //mObj.WaitOne();
+                    Thread.Sleep(40);
+                    x = Math.Sin(y);
+                    //mObj.ReleaseMutex();
+                }
+            //});
+        }
+    }
+}
